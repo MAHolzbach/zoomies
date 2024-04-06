70d9369c5a1ff38895f98a74ae94a720e40507e4
MAHolzbach <maholzbach@gmail.com>
2024-04-06 12:32:40 +0200

    Adding Stream

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
+import { StreamVideoProvider } from "@/providers/StreamClientProvider";
 import { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <>{children}</>;
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
 };
 
 export default RootLayout;
