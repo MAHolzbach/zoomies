9b70fb8620d33143e7285148a623b7d672e8990d
MAHolzbach <maholzbach@gmail.com>
2024-04-15 14:07:23 +0200

    .

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+
 const MeetingRoom = () => {
-  return <div>MeetingRoom</div>;
+  const [layout, setLayout] = useState("speaker-left");
+
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center"></div>
+      </div>
+    </section>
+  );
 };
 
 export default MeetingRoom;
