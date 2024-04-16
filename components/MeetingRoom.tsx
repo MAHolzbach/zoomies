f1e68b594fd8b55c467ac93d40eee741ccfd5e18
MAHolzbach <maholzbach@gmail.com>
2024-04-16 13:39:45 +0200

    No rendering

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,12 +1,35 @@
+import { cn } from "@/lib/utils";
+import { CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
 import { useState } from "react";
 
+type TCallLayout = "grid" | "speaker-left" | "speaker-right";
+
 const MeetingRoom = () => {
-  const [layout, setLayout] = useState("speaker-left");
+  const [layout, setLayout] = useState<TCallLayout>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
+
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
+
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  };
 
   return (
     <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
       <div className="relative flex size-full items-center justify-center">
-        <div className="flex size-full max-w-[1000px] items-center"></div>
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayout />
+        </div>
+        <div className={cn("h-[calc(100vh-86px)] hidden ml-2", { "show-block": showParticipants })}>
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
       </div>
     </section>
   );
