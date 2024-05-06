c9ed5b5feede8e8b1e07c35cb604451673016d0f
MAHolzbach <maholzbach@gmail.com>
2024-05-06 07:03:15 -0400

    auth issue?

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,14 +1,28 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import {
+  CallControls,
+  CallParticipantsList,
+  CallingState,
+  PaginatedGridLayout,
+  SpeakerLayout,
+  useCallStateHooks,
+} from "@stream-io/video-react-sdk";
 import { useState } from "react";
+import StreamLoader from "./StreamLoader";
 
 type TCallLayout = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
   const [layout, setLayout] = useState<TCallLayout>("speaker-left");
   const [showParticipants, setShowParticipants] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
+
+  //! callingState is "idle". So permaspinner and no indiciation I am "in a call". Dashboard for stream-io shows no activity. Not auth-ed correctly? Or at all?
+  const callingState = useCallCallingState();
+  console.log(callingState);
+  if (callingState !== CallingState.JOINED) return <StreamLoader />;
 
   const CallLayout = () => {
     switch (layout) {
